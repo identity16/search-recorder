525d7618f5d23ea09d30aa1dde388d3d73270f6c
identity16 <dnjswns0930@gmail.com>
2019-06-17 20:07:21 +0900

    아이콘 변경 및 클릭 이벤트 적용

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,12 +1,38 @@
+var isRecording = false;
+
 // 설치 시,
 chrome.runtime.onInstalled.addListener(function () {
 });
 
 // 페이지 로드 시, 
 chrome.webNavigation.onCompleted.addListener(function (data) {
+    if(!isRecording) return;
+
     var parsedUrl = getJsonFromUrl(data.url);
     console.log('history: ' + parsedUrl['q']);
 }, { url: [{ hostContains: 'www.google.', pathSuffix: 'search' }] });
+
+
+// 아이콘 클릭 시,
+chrome.browserAction.onClicked.addListener(function() {
+    if(isRecording) {   // 기록 종료
+        // TODO: 팝업으로 기록된 내용 표시
+        // TODO: 변수 초기화
+    }
+
+    isRecording = !isRecording;
+    
+    // 상태에 따라 아이콘 변경
+    var iconName = isRecording ? 'icon-on' : 'icon-off';
+    chrome.browserAction.setIcon({
+        path: {
+            "16": "images/"+ iconName +"-16.png",
+            "32": "images/"+ iconName +"-32.png",
+            "48": "images/"+ iconName +"-48.png",
+            "64": "images/"+ iconName +"-64.png"
+        }
+    })
+})
 
 // URL에서 querystrinng을 읽어 JSON 형태로 리턴
 function getJsonFromUrl(url) {
